6b3a15339ecf6613537f549a239ce861cf5c68dc
Marciliano Chavez <marcilianochaveziii@gmail.com>
2023-11-20 23:18:20 +0800

    tranferring code wip

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'user-login',
+    loadChildren: () =>
+      import('./user-login/user-login.module').then((m) => m.UserLoginModule),
+  },
+  {
+    path: 'signup',
+    loadChildren: () =>
+      import('./signup/signup.module').then((m) => m.SignupModule),
+  },
+  {
+    path: 'admin-login',
+    loadChildren: () =>
+      import('./admin-login/admin-login.module').then(
+        (m) => m.AdminLoginModule
+      ),
+  },
+  {
+    path: 'user-profile',
+    loadChildren: () =>
+      import('./user-profile/user-profile.module').then(
+        (m) => m.UserProfileModule
+      ),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
